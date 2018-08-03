d5fa47efb844daebdc5a845bb529447c282ec726
ekhoshnood <e.khoshnood966@gmail.com>
2018-08-03 11:13:56 +0430

    Working on originally

--- app/src/main/java/com/example/a259/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/a259/swoosh/LeagueActivity.kt
@@ -3,12 +3,10 @@ package com.example.a259.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class WelcomeActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
-
-        
+        setContentView(R.layout.activity_league)
     }
 }
