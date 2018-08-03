441a83fbf28d3305515bd80af8bb1269b1d7a95b
ekhoshnood <e.khoshnood966@gmail.com>
2018-08-03 12:58:40 +0430

    Working on branch

---
+++ app/src/main/java/com/example/a259/swoosh/BaseActivity.kt
@@ -0,0 +1,45 @@
+package com.example.a259.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
+
+open class  BaseActivity : AppCompatActivity() {
+
+    val tag = "lifecycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(tag, "${javaClass.simpleName} oncreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(tag, "${javaClass.simpleName} onstart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(tag, "${javaClass.simpleName} onresume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(tag, "${javaClass.simpleName} onpause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(tag, "${javaClass.simpleName} onstop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(tag, "${javaClass.simpleName} onstop")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d(tag, "${javaClass.simpleName} onstop")
+        super.onRestart()
+    }
+}
